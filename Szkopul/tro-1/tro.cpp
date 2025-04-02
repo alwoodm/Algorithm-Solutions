0de893288cc4e540f637c8efd597a378ae4f3bfc
alwoodm <110826071+alwoodm@users.noreply.github.com>
2025-04-02 12:59:35 +0200

    Triangle Perimeter

---
+++ Szkopul/tro-1/tro.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    unsigned long long a, b, c, answer;
+    cin >> a >> b >> c;
+    answer = a + b + c;
+    cout << answer;
+    return 0;
+}
