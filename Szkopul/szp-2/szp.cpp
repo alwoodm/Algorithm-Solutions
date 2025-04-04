36fb8cac0cd4b0fff9d047785d94c8f3b6487218
alwoodm <oleklisalwood@proton.me>
2025-04-04 17:06:43 +0200

    Pioter's Cipher

---
+++ Szkopul/szp-2/szp.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string key, word;
+    getline(cin, key);
+    getline(cin, word);
+
+    for (int i = 0; i < word.length(); i++) {
+        for (int j = 0; j < key.length()-1; j+=3) {
+                    if(word[i]==key[j])word[i] = key[j+1];
+                    else if(word[i]==key[j+1]) word[i] = key[j];
+            }
+        }
+    cout << word;
+    return 0;
+}
