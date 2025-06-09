83f94211ad3587afc3022952bb99e8efa106eefb
alwoodm <oleklisalwood@proton.me>
2025-06-09 16:05:06 +0200

    Codeforces 158A Next Round

---
+++ Codeforces/158A.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main() {
+    std::ios_base::sync_with_stdio(0);
+    std::cin.tie(0);
+    std::cout.tie(0);
+
+    int n, k, x=0;
+    std::cin >> n >> k;
+
+    int scores[n];
+    for (int i = 0; i < n; i++) {
+        std::cin >> scores[i];
+    }
+
+    int min_score = scores[k-1];
+
+    for (int i = 0; i < n; i++) {
+        if (scores[i] >= min_score && scores[i]!=0) x++;
+    }
+
+    std::cout << x;
+    return 0;
+}
